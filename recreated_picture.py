48bb9c8a6727747841da310c500009d4dd2df313
liana <hilllia@ssfs.org>
2019-12-17 13:31:18 -0500

    finished drawing creation/daily exercises - unit 10

--- recreated_picture.py
+++ recreated_picture.py
@@ -19,33 +19,29 @@ def make_star():
     pygame.draw.circle(main_surface, (209, 209, 209), (x, y), 3, 0)
 
 
-def building(x, y):
+def building(p):
+    x = p[0]
+    y = p[1]
     pygame.draw.rect(main_surface, (0, 0, 0), (x, y, 70, 500), 0)
 
 
-def window(x, y):
+def window(p):
+    x = p[0]
+    y = p[1]
     pygame.draw.rect(main_surface, (255, 220, 0), (x, y, 13, 13), 0)
 
 
 def main():
     for x in range(80):
         make_star()
-    building(0, 345) # building furthest to the left
-    building(40, 250)
-    building(100, 375) # third building
-    building(160, 330)
-    building(210, 225)
-    building(280, 430)
-    building(330, 350)
-    building(400, 250)
-    building(470, 400)
+    building_points = [(0, 345), (40, 250), (100, 375), (160, 330), (210, 225), (280, 430), (330, 350), (400, 250),
+                       (470, 400)]
+    for x in building_points:
+        building(x)
+    window_points = [(55, 300), (85, 320), (55, 400), (85, 400), (420, 300), (445, 350)]
+    for y in window_points:
+        window(y)
     pygame.draw.ellipse(main_surface, (110, 110, 110), (120, 250, 240, 600), 23)
-    window(55, 300)
-    window(85, 320)
-    window(55, 400)
-    window(85, 400)
-    window(420, 300)
-    window(445, 350)
 
 
 main()
@@ -57,4 +53,3 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-
